8c527491a4ad184ee89714862d01afc46fc24f0c
Gus Beane <angus.beane@cfa.harvard.edu>
2020-02-19 20:38:47 -0600

    Add R0 to gas fraction computation

--- plots/gas_fraction/compute_gas-fraction.py
+++ plots/gas_fraction/compute_gas-fraction.py
@@ -7,7 +7,9 @@ import pickle
 from joblib import Parallel, delayed
 
 def compute_gas_fraction(path, name, skip=10, output_dir='data/',
-                         Rmax=20, zmax=3, gas_ptype=0, star_ptype=(2,3,4),
+                         Rmax=20, zmax=3, 
+                         R0=8.2, dR=1.0,
+                         gas_ptype=0, star_ptype=(2,3,4),
                          center = np.array([200, 200, 200])):
     # try loading snapshot
     nsnap = len(glob.glob(path+'/output/snapdir*/*.0.hdf5'))
@@ -17,17 +19,21 @@ def compute_gas_fraction(path, name, skip=10, output_dir='data/',
         sn = arepo.Snapshot(path+'/output/', snapnum, combineFiles=True)
         time = sn.header.time.as_unit(3.1536E13 * arepo.u.second).value # in Myr
 
-        gas_mass = get_mass_in_ptype(sn, gas_ptype, Rmax, zmax, center)
-        star_mass = get_mass_in_ptype(sn, star_ptype, Rmax, zmax, center)
+        gas_mass = get_mass_in_ptype(sn, gas_ptype, 0., Rmax, 0., zmax, center)
+        star_mass = get_mass_in_ptype(sn, star_ptype, 0., Rmax, 0., zmax, center)
 
-        gas_fraction = gas_mass/(gas_mass + star_mass)
+        gas_fraction_disk = gas_mass/(gas_mass + star_mass)
 
-        out.append([time, gas_fraction])
-        print(time, gas_fraction)
+        gas_mass = get_mass_in_ptype(sn, gas_ptype, R0-dR/2., R0+dR/2., 0., zmax, center)
+        star_mass = get_mass_in_ptype(sn, star_ptype, R0-dR/2., R0+dR/2., 0., zmax, center)
+
+        gas_fraction_R0 = gas_mass/(gas_mass + star_mass)
+
+        out.append([time, gas_fraction_disk, gas_fraction_R0])
 
     pickle.dump(np.array(out), open(output_dir+'gas-fraction_'+name+'.p', 'wb'))
 
-def get_mass_in_ptype(snap, ptype, Rmax, zmax, center):
+def get_mass_in_ptype(snap, ptype, Rmin, Rmax, zmin, zmax, center):
     if isinstance(ptype, int):
         ptype = (ptype,)
 
