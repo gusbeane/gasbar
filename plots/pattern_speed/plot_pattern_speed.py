fb4c9bf9ad48fc97a34963558ec17ba8c073fbb2
Gus Beane <angus.beane@cfa.harvard.edu>
2020-03-16 15:22:08 -0400

    Update pattern speed for cored plots

--- plots/pattern_speed/plot_pattern_speed.py
+++ plots/pattern_speed/plot_pattern_speed.py
@@ -27,29 +27,43 @@ def plot_pattern_speed(name_list, c_list, ls_list, fout, n=5):
     fig.savefig(fout)
 
 if __name__ == '__main__':
+    fid_g1 = 'fid-disp1.0-fg0.1'
+    fid_g1_fixed1kpc = 'fid-disp1.0-fixedDisk-core1kpc'
+    fid_g1_fixed2kpc = 'fid-disp1.0-fixedDisk-core2kpc'
+    fid_g1_fixed3kpc = 'fid-disp1.0-fixedDisk-core3kpc'
+    fid_g1_fixed4kpc = 'fid-disp1.0-fixedDisk-core4kpc' 
+
     name_list = ['nbody-lvl5', 'nbody-lvl4', 'nbody-lvl3']
-    c_list = [None, None, None]
-    ls_list = [None, None, None]
+    c_list = [None, None, None, None, None]
+    ls_list = [None, None, None, None, None]
     plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_nbody.pdf')
 
-    name_list = ['wet-lvl5', 'wet-lvl4', 'wet-lvl3']
-    plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_wet.pdf')
+    # name_list = ['wet-lvl5', 'wet-lvl4', 'wet-lvl3']
+    # plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_wet.pdf')
 
-    name_list = ['fid-lvl5', 'fid-lvl4', 'fid-lvl3']
-    plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_fid.pdf')
+    # name_list = ['fid-lvl5', 'fid-lvl4', 'fid-lvl3']
+    # plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_fid.pdf')
 
-    name_list = ['nbody-lvl5', 'wet-lvl5', 'fid-lvl5']
-    plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_lvl5.pdf')
+    # name_list = ['nbody-lvl5', 'wet-lvl5', 'fid-lvl5']
+    # plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_lvl5.pdf')
     
-    name_list = ['nbody-lvl4', 'wet-lvl4', 'fid-lvl4']
-    plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_lvl4.pdf')
+    # name_list = ['nbody-lvl4', 'wet-lvl4', 'fid-lvl4']
+    # plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_lvl4.pdf')
     
-    name_list = ['nbody-lvl3', 'wet-lvl3', 'fid-lvl3']
-    plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_lvl3.pdf')
+    # name_list = ['nbody-lvl3', 'wet-lvl3', 'fid-lvl3']
+    # plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_lvl3.pdf')
 
-    name_list = ['nbody-lvl5', 'fid-disp1.0-fixedDisk-lvl5']
+    # name_list = ['nbody-lvl5', 'fid-disp1.0-fixedDisk-lvl5']
+    # plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_fixedDisk_lvl5.pdf')
+    
+    # name_list = ['nbody-lvl4', 'fid-disp1.0-fixedDisk-lvl4']
+    # plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_fixedDisk_lvl4.pdf')
+
+    name_list = [fid_g1+'-lvl5', fid_g1_fixed1kpc+'-lvl5', fid_g1_fixed2kpc+'-lvl5', fid_g1_fixed3kpc+'-lvl5',
+                 fid_g1_fixed4kpc+'-lvl5']
     plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_fixedDisk_lvl5.pdf')
-    
-    name_list = ['nbody-lvl4', 'fid-disp1.0-fixedDisk-lvl4']
+
+    name_list = [fid_g1+'-lvl4', fid_g1_fixed1kpc+'-lvl4', fid_g1_fixed2kpc+'-lvl4', fid_g1_fixed3kpc+'-lvl4',
+                 fid_g1_fixed4kpc+'-lvl4']
     plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_fixedDisk_lvl4.pdf')
 
