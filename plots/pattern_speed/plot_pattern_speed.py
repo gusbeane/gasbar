1b7fcc5964789a924da583c9fdc85137b56492d4
Gus Beane <angus.beane@cfa.harvard.edu>
2020-04-22 17:14:26 -0400

    Update pattern speed plots

--- plots/pattern_speed/plot_pattern_speed.py
+++ plots/pattern_speed/plot_pattern_speed.py
@@ -79,6 +79,9 @@ if __name__ == '__main__':
                  fid_g1_fixed6kpc+'-lvl4']
     plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_fixedDisk_lvl4.pdf', vline=1.5352*1000)
 
+    name_list = [nbody+'-lvl5', fid_g1+'-lvl5', fid_g1_dS_out_delay+'-lvl5']
+    plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_AGBdelay_lvl5.pdf')
+    
     name_list = [nbody+'-lvl4', fid_g1+'-lvl4', fid_g1_dS_out_delay+'-lvl4']
     plot_pattern_speed(name_list, c_list, ls_list, 'pattern_speed_AGBdelay_lvl4.pdf')
     
