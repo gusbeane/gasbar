ca9f6ef81a026bbc46edcc79c6cadc05afb4a1d1
Gus Beane <angus.beane@cfa.harvard.edu>
2020-04-24 16:45:14 -0400

    Some additions to toomre plots

--- plots/toomre/plot_toomre2.py
+++ plots/toomre/plot_toomre2.py
@@ -15,7 +15,7 @@ def plot_toomre2(name, fout):
     Q2_4 = dat[:,5]
 
     fac = 1.0
-    fac = np.sqrt(1.5)
+    #fac = np.sqrt(1.5)
 
     ax.plot(R, fac*Q, label='star', c='k', ls='dashed')
     ax.plot(R, fac*Q2_1, label='1e1')
@@ -36,9 +36,38 @@ def plot_toomre2(name, fout):
     fig.tight_layout()
     fig.savefig(fout)
 
+def plot_toomre_ratio(name, fout):
+    fig, ax = plt.subplots(1, 1)
+
+    fdata = 'data/' + name + '-Q2.npy'
+    dat = np.load(fdata)
+
+    R = dat[:,0]
+    Q = dat[:,1]
+    Q2_1 = dat[:,2]
+    Q2_2 = dat[:,3]
+    Q2_3 = dat[:,4]
+    Q2_4 = dat[:,5]
+
+    ax.plot(R, Q2_1/Q, label='1e1')
+    ax.plot(R, Q2_2/Q, label='1e2')
+    ax.plot(R, Q2_3/Q, label='1e3')
+    ax.plot(R, Q2_4/Q, label='1e4')
+    ax.axhline(1, c='k')
+
+    ax.set_xlabel('R [kpc]')
+    ax.set_ylabel('Q2/Q')
+    ax.legend(title='T [K]', frameon=False)
+
+
+
+    fig.tight_layout()
+    fig.savefig(fout)
+
 if __name__ == '__main__':
 
     name = 'fid-disp1.0-fg0.1-lvl5'
 
     plot_toomre2(name, 'toomre_two_comp.pdf')
+    plot_toomre_ratio(name, 'toomre_two_comp_ratio.pdf')
 
