5ec13c0388e6afba30ed6c249237d8f2000b66cd
Gus Beane <angus.beane@cfa.harvard.edu>
2020-04-29 15:26:11 -0400

    Update toomre2 comp

--- plots/toomre/compute_toomre2.py
+++ plots/toomre/compute_toomre2.py
@@ -28,6 +28,7 @@ def _Q2_of_k_(k, cs, kappa, sigmaR, sigmaStar, sigmaGas):
 def _toomre2_(kmin, kmax, cs, kappa, sigmaR, sigmaStar, sigmaGas):
     fun_list = []
     x_list = []
+    print('cs=', cs, 'kappa=', kappa, 'sigmaR=', sigmaR, 'sigmaStar=', sigmaStar, 'sigmaGas=', sigmaGas)
     for kguess in np.logspace(np.log10(kmin), np.log10(kmax), 10):
         ans = minimize(_Q2_of_k_, kguess, (cs, kappa, sigmaR, sigmaStar, sigmaGas), bounds=((kmin, kmax),))
         fun_list.append(ans.fun[0])
@@ -47,37 +48,22 @@ def compute_two_comp_toomre(path, name, output_dir='data/'):
     dat = np.genfromtxt(path+'/ICs/MW_ICs.vc2comp', names=True)
 
     gamma = 5./3.
-    u1 = compute_u_from_T(1e1)
-    u2 = compute_u_from_T(1e2)
-    u3 = compute_u_from_T(1e3)
-    u4 = compute_u_from_T(1e4)
-    cs2_1 = gamma * (gamma-1) * u1
-    cs2_2 = gamma * (gamma-1) * u2
-    cs2_3 = gamma * (gamma-1) * u3
-    cs2_4 = gamma * (gamma-1) * u4
+    u = compute_u_from_T(1e4)
+    cs2 = gamma * (gamma-1) * u
 
     kmin = 0.06
     kmax = 6000.0
 
     Q_star = []
-    Q2_T1e1 = []
-    Q2_T1e2 = []
-    Q2_T1e3 = []
-    Q2_T1e4 = []
+    Q2_list = []
     for kappa, sigmaR, sigmaStar, sigmaGas in zip(tqdm(dat['kappa']), dat['sigmaR'], dat['sigmaStar'], dat['sigmaGas']):
         Q = _toomre_star_(kappa, sigmaR, sigmaStar)
-        Q2_1, k = _toomre2_(kmin, kmax, np.sqrt(cs2_1), kappa, sigmaR, sigmaStar, sigmaGas)
-        Q2_2, k = _toomre2_(kmin, kmax, np.sqrt(cs2_2), kappa, sigmaR, sigmaStar, sigmaGas)
-        Q2_3, k = _toomre2_(kmin, kmax, np.sqrt(cs2_3), kappa, sigmaR, sigmaStar, sigmaGas)
-        Q2_4, k = _toomre2_(kmin, kmax, np.sqrt(cs2_4), kappa, sigmaR, sigmaStar, sigmaGas)
+        Q2, k = _toomre2_(kmin, kmax, np.sqrt(cs2), kappa, sigmaR, sigmaStar, sigmaGas)
         
         Q_star.append(Q)
-        Q2_T1e1.append(Q2_1)
-        Q2_T1e2.append(Q2_2)
-        Q2_T1e3.append(Q2_3)
-        Q2_T1e4.append(Q2_4)
+        Q2_list.append(Q2)
 
-    np.save(output_dir+name+'-Q2.npy', np.transpose([dat['R'], Q_star, Q2_T1e1, Q2_T1e2, Q2_T1e3, Q2_T1e4]))
+    np.save(output_dir+name+'-Q2.npy', np.transpose([dat['R'], Q_star, Q2_list]))
 
 
 if __name__ == '__main__':
@@ -85,10 +71,11 @@ if __name__ == '__main__':
     basepath = '../../runs/'
 
     fid_g1 = 'fid-disp1.0-fg0.1'
+    fid_d15_g1 = 'fid-disp1.5-fg0.1'
 
     # look to see if we are on my macbook or on the cluster
     if sys.platform == 'darwin':
-        pair_list = [(fid_g1, 'lvl5')]
+        pair_list = [(fid_g1, 'lvl5'), (fid_d15_g1, 'lvl5')]
     else:
         pair_list = [(fid_g1, 'lvl5'), (fid_g1, 'lvl4'), (fid_g1, 'lvl3')]
 
--- plots/toomre/plot_toomre2.py
+++ plots/toomre/plot_toomre2.py
@@ -1,37 +1,30 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
 
-def plot_toomre2(name, fout):
+def plot_toomre2(name_list, fout):
     fig, ax = plt.subplots(1, 1)
 
-    fdata = 'data/' + name + '-Q2.npy'
-    dat = np.load(fdata)
+    for i, name in enumerate(name_list):
 
-    R = dat[:,0]
-    Q = dat[:,1]
-    Q2_1 = dat[:,2]
-    Q2_2 = dat[:,3]
-    Q2_3 = dat[:,4]
-    Q2_4 = dat[:,5]
+        fdata = 'data/' + name + '-Q2.npy'
+        dat = np.load(fdata)
 
-    fac = 1.0
-    #fac = np.sqrt(1.5)
+        R = dat[:,0]
+        Q = dat[:,1]
+        Q2 = dat[:,2]
 
-    ax.plot(R, fac*Q, label='star', c='k', ls='dashed')
-    ax.plot(R, fac*Q2_1, label='1e1')
-    ax.plot(R, fac*Q2_2, label='1e2')
-    ax.plot(R, fac*Q2_3, label='1e3')
-    ax.plot(R, fac*Q2_4, label='1e4')
+        if i==0:
+            ax.plot(R, Q, label='star', c='k', ls='dashed')
+        ax.plot(R, Q2, label=name)
+    
     ax.axhline(1, c='k')
 
-    ax.set_yscale('log')
-
     ax.set_xlabel('R [kpc]')
-    ax.set_ylabel('Q2')
-    ax.legend(title='T [K]', frameon=False)
+    ax.set_ylabel('Q')
+    ax.legend(frameon=False)
 
-    ax.set_ylim(0.06, 5)
-
+    ax.set_xlim(0, 10)
+    ax.set_ylim(0, 10)
 
     fig.tight_layout()
     fig.savefig(fout)
@@ -66,8 +59,11 @@ def plot_toomre_ratio(name, fout):
 
 if __name__ == '__main__':
 
-    name = 'fid-disp1.0-fg0.1-lvl5'
+    fid_g1 = 'fid-disp1.0-fg0.1-lvl5'
+    fid_d15_g1 = 'fid-disp1.5-fg0.1-lvl5'
+   
+    name_list = [fid_g1, fid_d15_g1]
 
-    plot_toomre2(name, 'toomre_two_comp.pdf')
-    plot_toomre_ratio(name, 'toomre_two_comp_ratio.pdf')
-
+    plot_toomre2(name_list, 'toomre_two_comp.pdf')
+    #plot_toomre_ratio(fid_g1, 'toomre_two_comp_ratio_'+fid_g1+'.pdf')
+    
