b38807f663781ab96ac92343080f233480e38432
Gus Beane <angus.beane@cfa.harvard.edu>
2020-05-20 01:17:45 -0400

    Update rsync_data.sh

--- plots/movie_5panel/rsync_data.sh
+++ plots/movie_5panel/rsync_data.sh
@@ -2,4 +2,5 @@
 
 #rsync -azvp --progress 'cannon:/n/home01/abeane/starbar/plots/movie_5panel-fg/movies/*.mp4' movies/
 rsync -azvp --progress 'cannon:/n/home01/abeane/starbar-correcth/plots/movie_5panel/movies/*.mp4' movies/
+rsync -azvp --progress 'cannon:/n/home01/abeane/starbar-correcth/plots/movie_5panel/profiles/*.prof' profiles/
 
