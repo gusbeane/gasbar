0563426718893847adbff01ee105198fd796ea39
Gus Beane <angus.beane@cfa.harvard.edu>
2020-05-26 16:59:28 -0400

    Small changes to scripts

--- plots/movie_5panel/job_movie.sh
+++ plots/movie_5panel/job_movie.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#SBATCH -p itc_cluster,shared,conroy,hernquist
+#SBATCH -p hernquist,conroy
 #SBATCH -J movie 
-#SBATCH -n 14 
+#SBATCH -n 64 
 #SBATCH -N 1
 #SBATCH -o OUTPUT_frames.%j.out
 #SBATCH -e ERROR_frames.%j.err
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --mem-per-cpu=12000
+#SBATCH --mem-per-cpu=3900
 ##SBATCH -t 4-00:00           # Runtime in D-HH:MM
 #SBATCH -t 7-00:00           # Runtime in D-HH:MM
 
--- plots/movie_5panel/run_projections.py
+++ plots/movie_5panel/run_projections.py
@@ -14,10 +14,10 @@ fid_dP_sEQ_g1 = 'fid-dispPoly-fg0.1-SoftEQS0'
 fid_dP_rx_g1 = 'fid-dispPoly-fg0.1-MeshReg'
 
 pair_list = [(fid_dP_sEQ_g1, 'lvl5'), (fid_dP_sEQ_g1, 'lvl4'), (fid_dP_sEQ_g1, 'lvl3'),
-	  	     (fid_dP_sEQ_g1, 'lvl5'), (fid_dP_sEQ_g1, 'lvl4'), (fid_dP_sEQ_g1, 'lvl3')]
+	  	     (fid_dP_rx_g1, 'lvl5'), (fid_dP_rx_g1, 'lvl4'), (fid_dP_rx_g1, 'lvl3')]
 
 name_list = [           p[0] + '-' + p[1] for p in pair_list]
 path_list = [basepath + p[0] + '/' + p[1] for p in pair_list]
                                            
-for fout, path, nsnap in zip(fout_list, path_list, nsnap_list):
+for name, path in zip(name_list, path_list):
     construct_update_projection_hdf5(name, path, nproc=nproc)
