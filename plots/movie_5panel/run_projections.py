ad1d748edba826ea303aa2447869fb1094668716
Gus Beane <angus.beane@cfa.harvard.edu>
2020-07-07 15:34:06 -0400

    Add new runs to run_projections.py

--- plots/movie_5panel/run_projections.py
+++ plots/movie_5panel/run_projections.py
@@ -13,10 +13,16 @@ basepath = '../../runs/'
 fid_dP_sEQ_g1 = 'fid-dispPoly-fg0.1-SoftEQS0'
 fid_dP_rx_g1 = 'fid-dispPoly-fg0.1-MeshReg'
 fid_dP_rx_g1_dAGB = 'fid-dispPoly-fg0.1-MeshReg-diskAGB'
+fid_dP_rx_g1_dAGB_delay = 'fid-dispPoly-fg0.1-MeshReg-diskAGB-delay1.25'
+fid_dP_rx_g1_Rcore = 'fid-dispPoly-fg0.1-MeshReg-Rcore1.0'
+fid_dP_rx_g1_Rcore_AGB = 'fid-dispPoly-fg0.1-MeshReg-Rcore1.0-diskAGB'
+fid_dP_rx_g1_Rcore14_AGB = 'fid-dispPoly-fg0.1-MeshReg-Rcore1.4-diskAGB'
 
-pair_list = [(fid_dP_sEQ_g1, 'lvl5'), (fid_dP_sEQ_g1, 'lvl4'), (fid_dP_sEQ_g1, 'lvl3'),
-	  	     (fid_dP_rx_g1, 'lvl5'), (fid_dP_rx_g1, 'lvl4'), (fid_dP_rx_g1, 'lvl3'),
-             (fid_dP_rx_g1_dAGB, 'lvl3')]
+pair_list = [(fid_dP_sEQ_g1, 'lvl4'), (fid_dP_sEQ_g1, 'lvl3'),
+	  	     (fid_dP_rx_g1, 'lvl4'), (fid_dP_rx_g1, 'lvl3'),
+             (fid_dP_rx_g1_Rcore, 'lvl4'), (fid_dP_rx_g1_Rcore, 'lvl3'),
+             (fid_dP_rx_g1_Rcore_AGB, 'lvl4'), (fid_dP_rx_g1_Rcore_AGB, 'lvl3'),
+             (fid_dP_rx_g1_Rcore14_AGB, 'lvl4')]#, (fid_dP_rx_g1_Rcore14_AGB, 'lvl3')]
 
 name_list = [           p[0] + '-' + p[1] for p in pair_list]
 path_list = [basepath + p[0] + '/' + p[1] for p in pair_list]
