7e8de16b1ea611fd65a982641e9a69f79aa841d9
Gus Beane <angus.beane@cfa.harvard.edu>
2020-11-27 16:27:02 -0500

    Update movie making files for reruns

--- plots/movie_5panel/run_projections.py
+++ plots/movie_5panel/run_projections.py
@@ -10,19 +10,9 @@ except:
 
 basepath = '../../runs/'
 
-fid_dP_sEQ_g1 = 'fid-dispPoly-fg0.1-SoftEQS0'
-fid_dP_rx_g1 = 'fid-dispPoly-fg0.1-MeshReg'
-fid_dP_rx_g1_dAGB = 'fid-dispPoly-fg0.1-MeshReg-diskAGB'
-fid_dP_rx_g1_dAGB_delay = 'fid-dispPoly-fg0.1-MeshReg-diskAGB-delay1.25'
-fid_dP_rx_g1_Rcore = 'fid-dispPoly-fg0.1-MeshReg-Rcore1.0'
-fid_dP_rx_g1_Rcore_AGB = 'fid-dispPoly-fg0.1-MeshReg-Rcore1.0-diskAGB'
-fid_dP_rx_g1_Rcore14_AGB = 'fid-dispPoly-fg0.1-MeshReg-Rcore1.4-diskAGB'
+fid_dP = 'fRpoly'
 
-pair_list = [(fid_dP_sEQ_g1, 'lvl4'), (fid_dP_sEQ_g1, 'lvl3'),
-	  	     (fid_dP_rx_g1, 'lvl4'), (fid_dP_rx_g1, 'lvl3'),
-             (fid_dP_rx_g1_Rcore, 'lvl4'), (fid_dP_rx_g1_Rcore, 'lvl3'),
-             (fid_dP_rx_g1_Rcore_AGB, 'lvl4'), (fid_dP_rx_g1_Rcore_AGB, 'lvl3'),
-             (fid_dP_rx_g1_Rcore14_AGB, 'lvl4')]#, (fid_dP_rx_g1_Rcore14_AGB, 'lvl3')]
+pair_list = [(fid_dP, 'lvl5'), (fid_dP, 'lvl4'), (fid_dP, 'lvl3')]
 
 name_list = [           p[0] + '-' + p[1] for p in pair_list]
 path_list = [basepath + p[0] + '/' + p[1] for p in pair_list]
