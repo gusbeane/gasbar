b93207d044c10c35ecdfa02b40440a5be309f070
Gus Beane <angus.beane@cfa.harvard.edu>
2020-12-10 14:04:55 -0500

    Update runs for fourier component comp

--- plots/fourier_component/compute_fourier_component.py
+++ plots/fourier_component/compute_fourier_component.py
@@ -168,20 +168,26 @@ if __name__ == '__main__':
 
     basepath = '../../runs/'
 
+    Nbody = 'Nbody'
     fid_dP = 'fRpoly'
     fid_dP_c1 = 'fRpoly-Rcore1.0'
     fid_dP2_c1 = 'fRpoly2-Rcore1.0'
     fid_dP_c1_rx = 'fRpoly-Rcore1.0-relax'
     fid_dP_c1_bG = 'fRpoly-Rcore1.0-barGas'
     fid_dP_c1_bG1 = 'fRpoly-Rcore1.0-barGas1.0'
+    fid_dP_c1_bG2 = 'fRpoly-Rcore1.0-barGas2.0'
+    fid_dP_c1_sp = 'fRpoly-Rcore1.0-spring'
     fid_dP_c1_rB = 'fRpoly-Rcore1.0-ringBug'
     fid_dP_c1_h = 'fRpoly-Rcore1.0-hose-Del1.0-Rg15.0-Rate0.5-Rh0.2-Vel160.0'
     fid_dP_c1_h_v140 = 'fRpoly-Rcore1.0-hose-Del1.0-Rg15.0-Rate0.5-Rh0.2-Vel140.0'
 
 
     pair_list = [#(fid_dP, 'lvl5'), (fid_dP, 'lvl4'), #(fid_dP, 'lvl3'),
+                 (Nbody, 'lvl5'), (Nbody, 'lvl4'), (Nbody, 'lvl3'),
                  (fid_dP_c1, 'lvl5'), (fid_dP_c1, 'lvl4'), (fid_dP_c1, 'lvl3'),
-                 (fid_dP2_c1, 'lvl5'), (fid_dP2_c1, 'lvl4'), (fid_dP2_c1, 'lvl3')]
+                 (fid_dP2_c1, 'lvl5'), (fid_dP2_c1, 'lvl4'), (fid_dP2_c1, 'lvl3'),
+                 (fid_dP_c1_bG2, 'lvl5'), (fid_dP_c1_bG2, 'lvl4'), (fid_dP_c1_bG2, 'lvl3'),
+                 (fid_dP_c1_sp, 'lvl5'), (fid_dP_c1_sp, 'lvl4'), (fid_dP_c1_sp, 'lvl3')]
                  # (fid_dP_c1_bG, 'lvl5'), (fid_dP_c1_bG, 'lvl4'),# (fid_dP_c1_bG, 'lvl3'),
                  # (fid_dP_c1_bG1, 'lvl5'),# (fid_dP_c1_bG, 'lvl4'),# (fid_dP_c1_bG, 'lvl3'),
                  # (fid_dP_c1_rB, 'lvl5'), (fid_dP_c1_rB, 'lvl4'), (fid_dP_c1_rB, 'lvl3'),
