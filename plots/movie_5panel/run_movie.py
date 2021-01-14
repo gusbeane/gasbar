49d31b35e4d17a2272e4e542343068229d78f52e
Gus Beane <angus.beane@cfa.harvard.edu>
2021-01-14 13:59:09 -0500

    Add time to gas xy panel
    Needed when stitching together other movies, fine to have redundant info

--- plots/movie_5panel/run_movie.py
+++ plots/movie_5panel/run_movie.py
@@ -24,7 +24,7 @@ mov_starxy = 'movies/' + name + '_star_xy.mp4'
 mov_starxz = 'movies/' + name + '_star_xz.mp4'
 mov_4panel = 'movies/' + name + '_4panel.mp4'
 
-make_movie(proj, 0, 'xy', mov_gasxy, vmin=gas_vmin, vmax=gas_vmax)
+make_movie(proj, 0, 'xy', mov_gasxy, vmin=gas_vmin, vmax=gas_vmax, plot_time=True)
 make_movie(proj, 0, 'xz', mov_gasxz, vmin=gas_vmin, vmax=gas_vmax)
 make_movie(proj, [2, 3, 4], 'xy', mov_starxy, vmin=star_vmin, vmax=star_vmax)
 make_movie(proj, [2, 3, 4], 'xz', mov_starxz, vmin=star_vmin, vmax=star_vmax, plot_time=True)
