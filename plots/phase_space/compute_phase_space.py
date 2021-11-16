7354f1817c33fbd3f4eb1d99911c4a11bf7eb084
Gus Beane <angus.beane@cfa.harvard.edu>
2021-11-16 11:35:54 -0500

    Update phase space comp to be on chunks

--- plots/phase_space/compute_phase_space.py
+++ plots/phase_space/compute_phase_space.py
@@ -2,13 +2,11 @@ import numpy as np
 import arepo
 import sys
 from tqdm import tqdm
-import astropy.units as u
 import h5py as h5
 import glob
 import os
-from numba import njit
 import re
-from sklearn.cluster import KMeans
+import time
 
 from joblib import Parallel, delayed
 
@@ -18,82 +16,74 @@ def read_snap(path, idx, parttype=[0], fields=['Coordinates', 'Masses', 'Velocit
     
     return arepo.Snapshot(fname, idx, parttype=parttype, fields=fields, combineFiles=True)
 
-def construct_orbits(path, idx):
-    sn = read_snap(path, idx, parttype=[2])
+def _run_thread(path, name, idx_list, snap_id, id_chunks, data_dir='data_tmp/'):
     
-    id_list = sn.part2.id
-    keys = np.argsort(id_list)
-    
-    pos = sn.part2.pos.value
-    pos = pos[keys]
-    
-    vel = sn.part2.vel.value
-    vel = vel[keys]
-    
-    w = np.hstack((pos, vel))
-    t = sn.Time.value
-
-    return t, w
-
-def _run_thread(path, name, idx_list, snap_id, id_chunks):
     h5out_list = []
-    prefix = 'data_tmp/' + name + '/tmp' + str(snap_id)
+
+    Nsnap = len(idx_list)
+    
+    # Create a temporary directory which will store each chunk of ids as a separate file.
+    prefix = data_dir + name + '/tmp' + str(snap_id)
     if not os.path.isdir(prefix):
         os.mkdir(prefix)
+    
+    # Loop through each id chunk and create the file with temporary output.
     for i,id_chunk_list in enumerate(id_chunks):
+        Nids = len(id_chunk_list)
+
         fout = prefix + '/tmp' + str(i) + '.hdf5'
         h5out = h5.File(fout, mode='w')
+
+        pos = np.zeros((Nids, Nsnap, 3))
+        time = np.zeros(Nsnap)
+        
         h5out.create_dataset("ParticleIDs", data=id_chunk_list)
-
-        pos = np.zeros((len(id_chunk_list), len(idx_list), 3))
-
+        h5out.create_dataset("Time", data=time)
         h5out.create_dataset("Coordinates", data=pos)
         h5out.create_dataset("Velocities", data=pos)
 
-        time = np.zeros(len(idx_list))
-        h5out.create_dataset("Time", data=time)
-
         h5out_list.append(h5out)
     
+    # Now loop through each index, read the snapshot, then loop through each
+    # id chunk and write to the relevant file.
     for i,idx in enumerate(idx_list):
         sn = read_snap(path, idx, parttype=[2])
+        # Sort by ID
         key = np.argsort(sn.part2.id)
         pos_ = sn.part2.pos.value[key]
         vel_ = sn.part2.vel.value[key]
 
         for j,id_chunk_list in enumerate(id_chunks):
             in_key = np.isin(np.sort(sn.part2.id), id_chunk_list)
+
             h5out_list[j]['Coordinates'][:,i,:] = pos_[in_key]
             h5out_list[j]['Velocities'][:,i,:] = vel_[in_key]
             h5out_list[j]['Time'][i] = sn.Time.value
-        
+    
+    # Close h5 files.
     for i,_ in enumerate(id_chunks):
         h5out_list[i].close()
     
     return None
 
-def _concat_h5_files(name, chunk_id, id_chunk_list, indices_chunks, nsnap):
-    # first create dummy hdf5 files
-    prefix = 'data_tmp/' + name + '/tmp'
-
-    fout = 'data_tmp/' + name + '/phase_space_' + name + '.' + str(chunk_id) + '.hdf5'
+def _concat_h5_files(name, chunk_id, id_chunk_list, indices_chunks, nsnap, data_dir='data_tmp/'):
+    # First create hdf5 output file
+    fout = data_dir + name + '/phase_space_' + name + '.' + str(chunk_id) + '.hdf5'
     h5out = h5.File(fout, mode='w')
 
-    h5out.create_dataset("ParticleIDs", data=id_chunk_list)
+    # Temporary arrays for storing output
+    Nids = len(id_chunk_list)
 
-    pos = np.zeros((len(id_chunk_list), nsnap, 3))
-    vel = np.zeros((len(id_chunk_list), nsnap, 3))
+    pos = np.zeros((Nids, nsnap, 3))
+    vel = np.zeros((Nids, nsnap, 3))
     time = np.zeros(nsnap)
 
-    # h5out.create_dataset("Coordinates", data=pos)
-    # h5out.create_dataset("Velocities", data=pos)
+    # Prefix for temporary data files.
+    prefix = data_dir + name + '/tmp'
 
     for j,idx_list in enumerate(indices_chunks):
         fin = prefix + str(j) + '/tmp' + str(chunk_id) + '.hdf5'
         h5in = h5.File(fin, mode='r')
-
-        print(idx_list)
-        print(np.shape(h5in['Coordinates']))
 
         pos[:,idx_list,:] = np.array(h5in['Coordinates'])
         vel[:,idx_list,:] = np.array(h5in['Velocities'])
@@ -101,13 +91,14 @@ def _concat_h5_files(name, chunk_id, id_chunk_list, indices_chunks, nsnap):
 
         h5in.close()
 
+    h5out.create_dataset("ParticleIDs", data=id_chunk_list)
     h5out.create_dataset("Coordinates", data=pos)
     h5out.create_dataset("Velocities", data=vel)
     h5out.create_dataset("Time", data=time)
 
     h5out.close()
 
-def run(path, name, nsnap, nproc, nchunk):
+def get_id_indices_chunks(nsnap, path, nchunk, nproc):
     indices = np.arange(nsnap)
 
     sn = read_snap(path, indices[-1], parttype=[2])
@@ -118,60 +109,31 @@ def run(path, name, nsnap, nproc, nchunk):
 
     indices_chunks = np.array_split(indices, 4*nproc)
 
-    if not os.path.isdir('data_tmp/'+name):
-        os.mkdir('data_tmp/'+name)
+    return id_chunks, indices_chunks
 
+def run(path, name, nsnap, nproc, nchunk, data_dir='data_tmp/'):
+    
+    # Split up particle ids and snapshot indices into chunks to be processed individually
+    id_chunks, indices_chunks = get_id_indices_chunks(nsnap, path, nchunk, nproc)
+
+    # If output directory does not exist, make it
+    if not os.path.isdir(data_dir+name):
+        os.mkdir(data_dir+name)
+
+    # Runs through each chunk of indices and reads the snapshot of each index. Each chunk of ids is written to a different temporary file.
+    t0 = time.time()
     _ = Parallel(n_jobs=nproc) (delayed(_run_thread)(path, name, indices_chunks[i], i, id_chunks) for i in tqdm(range(len(indices_chunks))))
+    t1 = time.time()
+    print('First loop took', t1-t0, 's')
 
+    # Runs through each chunk of ids and reads the temporary files from the previous step, then writes a single file spanning all indices
+    # for each chunk of ids.
+    t0 = time.time()
     _ = Parallel(n_jobs=nproc) (delayed(_concat_h5_files)(name, i, id_chunks[i], indices_chunks, nsnap) for i in tqdm(range(len(id_chunks))))
+    t1 = time.time()
+    print('Second loop took', t1-t0, 's')
 
-    # concat_h5_files(name, id_chunks, indices_chunks, nsnap)
-
-    return None
-
-
-# def run(path, name, nsnap, nproc, nchunk):
-
-#     indices = np.arange(nsnap)
-    
-#     sn = read_snap(path, indices[-1], parttype=[2])
-#     ids = sn.part2.id
-#     ids = np.sort(ids)
-
-#     # open a file and load dummy data for each chunk of stars
-#     id_chunks = np.array_split(ids, nchunk)
-#     h5out_list = []
-#     for i,id_chunk_list in tqdm(enumerate(id_chunks)):
-#         fout = 'data/phase_space_' + name + '.' + str(i) + '.hdf5'
-#         h5out = h5.File(fout, mode='w')
-#         h5out.create_dataset("ParticleIDs", data=id_chunk_list)
-        
-#         pos = np.zeros((len(id_chunk_list), nsnap, 3))
-#         vel = np.zeros((len(id_chunk_list), nsnap, 3))
-
-#         h5out.create_dataset("Coordinates", data=pos)
-#         h5out.create_dataset("Velocities", data=vel)
-
-#         h5out_list.append(h5out)
-    
-#     # now load each snapshot and modify the h5 files
-
-#     for idx in tqdm(indices):
-#         sn = read_snap(path, idx, parttype=[2])
-#         key = np.argsort(sn.part2.id)
-#         pos_ = sn.part2.pos.value[key]
-#         vel_ = sn.part2.vel.value[key]
-
-#         for i,id_chunk_list in enumerate(id_chunks):
-#             in_key = np.isin(np.sort(sn.part2.id), id_chunk_list)
-#             h5out_list[i]['Coordinates'][:,idx,:] = pos_[in_key]
-#             h5out_list[i]['Velocities'][:,idx,:] = vel_[in_key]
-        
-#     for i,_ in enumerate(id_chunks):
-#         h5out_list[i].close()
-    
-#     return None
-    
+    return None    
 
 if __name__ == '__main__':
     nproc = int(sys.argv[1])
@@ -181,8 +143,8 @@ if __name__ == '__main__':
     Nbody = 'Nbody'
     phgvS2Rc35 = 'phantom-vacuum-Sg20-Rc3.5'
 
-    pair_list = [(Nbody, 'lvl4', 64), (Nbody, 'lvl3', 64*8)]
-                #  (phgvS2Rc35, 'lvl4'), (phgvS2Rc35, 'lvl3')]
+    pair_list = [(Nbody, 'lvl4', 64), (Nbody, 'lvl3', 64),
+                 (phgvS2Rc35, 'lvl4', 64), (phgvS2Rc35, 'lvl3', 64)]
 
     name_list   = [           p[0] + '-' + p[1] for p in pair_list]
     path_list   = [basepath + p[0] + '/' + p[1] for p in pair_list]
--- plots/phase_space/job_phase_space.sh
+++ plots/phase_space/job_phase_space.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#SBATCH -p hernquist,conroy,shared,itc_cluster
+#SBATCH -p itc_cluster,shared
 ##SBATCH --constraint=intel
 #SBATCH -J orbits 
-#SBATCH -n 48
+#SBATCH -n 16
 #SBATCH -N 1
 #SBATCH -o OUTPUT_frames.%j.out
 #SBATCH -e ERROR_frames.%j.err
@@ -11,7 +11,7 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-##SBATCH --mem-per-cpu=3900
+##SBATCH --mem-per-cpu=5500
 #SBATCH --mem=128G
 ##SBATCH -t 4-00:00           # Runtime in D-HH:MM
 #SBATCH -t 7-00:00           # Runtime in D-HH:MM
