24139799a914b263e10290c6eabafbe86ef6cf07
Gus Beane <angus.beane@cfa.harvard.edu>
2021-11-28 12:14:05 -0500

    Write processing of snap chunk

--- plots/phase_space/compute_phase_space.c
+++ plots/phase_space/compute_phase_space.c
@@ -3,9 +3,14 @@
 #include <unistd.h>
 #include <string.h>
 #include <hdf5.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <math.h>
 
 #define NTYPES 6
 #define IDX(i, j) i*3+j
+
+uint NumPart_Total_LastSnap[NTYPES];
 
 struct Part
 {
@@ -19,7 +24,7 @@ struct Part
 void compute_Nchunk(int *Nchunk_id, int *Nchunk_snap){
     // TODO: compute these according to memory requirements
     *Nchunk_id = 64;
-    *Nchunk_snap = 12;
+    *Nchunk_snap = 128;
     return;
 }
 
@@ -69,8 +74,6 @@ void read_parttype_ids(char *output_dir, int snap_idx, int PartType, long long *
     sprintf(dset_name, "/PartType%d/%s", PartType, "ParticleIDs");
 
     H5Fclose(file_id);
-
-    printf("fname=%s, Nfiles=%d\n", fname, Nfiles);
 
     // printf("grp_name=%s, dset_name=%s\n", grp_name, dset_name);
 
@@ -123,7 +126,7 @@ void read_parttype_vec(char *output_dir, int snap_idx, int PartType, char *prope
 
     H5Fclose(file_id);
 
-    printf("fname=%s, Nfiles=%d\n", fname, Nfiles);
+    // printf("fname=%s, Nfiles=%d\n", fname, Nfiles);
 
     // printf("grp_name=%s, dset_name=%s\n", grp_name, dset_name);
 
@@ -154,19 +157,23 @@ void read_parttype_vec(char *output_dir, int snap_idx, int PartType, char *prope
     return;
 }
 
-long long cmprID (const void *a, const void *b){
+int cmprID (const void *a, const void *b){
     struct Part *partA = (struct Part *)a;
     struct Part *partB = (struct Part *)b;
-    return (partA->ID - partB->ID);
+    if (partA->ID - partB->ID > 0)
+        return 1;
+    if (partA->ID - partB->ID < 0)
+        return -1;
+    return 0;
 }
 
-void get_part(char* basepath, int SnapIdx, int PartType, struct Part ** part)
+void get_part(char* output_dir, int SnapIdx, int PartType, struct Part ** part)
 {
     // read final snapshot
-    char fname[1000], output_dir[1000];
+    char fname[1000];
     uint NumPart_Total[NTYPES];
-    sprintf(fname, "%s/output/snapdir_%03d/snapshot_%03d.0.hdf5", basepath, SnapIdx, SnapIdx);
-    printf("fname=%s\n", fname);
+    sprintf(fname, "%s/snapdir_%03d/snapshot_%03d.0.hdf5", output_dir, SnapIdx, SnapIdx);
+    // printf("fname=%s\n", fname);
     hid_t file_id = H5Fopen(fname, H5F_ACC_RDONLY, H5P_DEFAULT);
 
     // read num part total
@@ -174,7 +181,6 @@ void get_part(char* basepath, int SnapIdx, int PartType, struct Part ** part)
     
     // read disk ids
     long long *DiskIDs;
-    sprintf(output_dir, "%s/output/", basepath);
     read_parttype_ids(output_dir, SnapIdx, PartType, &DiskIDs);
     
     // read phase space coordinates
@@ -185,7 +191,7 @@ void get_part(char* basepath, int SnapIdx, int PartType, struct Part ** part)
 
     *part = (struct Part *)malloc(sizeof(struct Part) * NumPart_Total[PartType]);
 
-    printf("DiskPos[0] = %g|%g|%g\n", DiskPos[IDX(0, 0)], DiskPos[IDX(0, 1)], DiskPos[IDX(0, 2)]);
+    // printf("DiskPos[0] = %g|%g|%g\n", DiskPos[IDX(0, 0)], DiskPos[IDX(0, 1)], DiskPos[IDX(0, 2)]);
 
     // load from separate arrays into structured array
     int i, j;
@@ -228,13 +234,13 @@ void array_split_int(int Nchunk, int *List, int NList, int ***OutList, int **Out
     int NperListMax = (NList - 1)/Nchunk + 1;
     int NLeftOver = NperListMax * Nchunk - NList;
 
-    printf("NperListMax=%d\n", NperListMax);
-    printf("NLeftOver=%d\n", NLeftOver);
+    // printf("NperListMax=%d\n", NperListMax);
+    // printf("NLeftOver=%d\n", NLeftOver);
 
     // Allocate and write down the number of items in each chunk
     *OutNPerList = (int *)malloc(sizeof(int) * Nchunk);
     for(int i=0; i<Nchunk; i++){
-        printf("i=%d\n", i);
+        // printf("i=%d\n", i);
         (*OutNPerList)[i] = NperListMax;
         if(i >= (Nchunk - NLeftOver))
             (*OutNPerList)[i] -= 1;
@@ -272,7 +278,7 @@ void array_split_llong(int Nchunk, long long *List, int NList, long long ***OutL
     long long NLeftOver = NperListMax * Nchunk - NList;
 
     // Allocate and write down the number of items in each chunk
-    printf("NperListMax=%lld\n", NperListMax);
+    // printf("NperListMax=%lld\n", NperListMax);
     *OutNPerList = (long long *)malloc(sizeof(long long) * Nchunk);
     for(int i=0; i<Nchunk; i++){
         (*OutNPerList)[i] = NperListMax;
@@ -283,11 +289,8 @@ void array_split_llong(int Nchunk, long long *List, int NList, long long ***OutL
     // verify
     long long chk = 0;
     for(int i=0; i<Nchunk; i++)
-    {
-        printf("i=%lld, chk=%lld, OutNPerList[i]=%lld\n", i, chk, (*OutNPerList)[i]);
         chk += (*OutNPerList)[i];
-    }
-    printf("chk=%lld, NList=%lld\n", chk, NList);
+    // printf("chk=%lld, NList=%lld\n", chk, NList);
     if(chk != NList)
         printf("WARNING chk=%lld is not equal to NList=%lld\n", chk, NList);
 
@@ -303,6 +306,210 @@ void array_split_llong(int Nchunk, long long *List, int NList, long long ***OutL
         memcpy((*OutList)[i], List + Ncum, sizeof(long long) * (*OutNPerList)[i]);
         Ncum += (*OutNPerList)[i];
     }
+}
+
+void sort_by_id(long long *chunk_ids, long long Nids_in_chunk, struct Part * part, double **out_pos, double **out_vel, double **out_acc)
+{
+    long long itot=0;
+    long long ichunk, chk_id;
+    int j;
+
+    for(ichunk=0; ichunk<Nids_in_chunk; ichunk++){
+        // printf("chk_id=%lld, part[itot].ID=%lld\n", chk_id, part[itot].ID);
+        chk_id = chunk_ids[ichunk];
+        while(chk_id > part[itot].ID)
+        {
+            // printf("itot=%d\n", itot);
+            itot++;
+        }
+
+        if(chk_id == part[itot].ID){
+            for(j=0; j<3; j++){
+                (*out_pos)[IDX(ichunk, j)] = part[itot].Pos[j];
+                (*out_vel)[IDX(ichunk, j)] = part[itot].Vel[j];
+                (*out_acc)[IDX(ichunk, j)] = part[itot].Acc[j];
+            }
+        }
+        else{
+            for(j=0; j<3; j++){
+                (*out_pos)[IDX(ichunk, j)] = NAN;
+                (*out_vel)[IDX(ichunk, j)] = NAN;
+                (*out_acc)[IDX(ichunk, j)] = NAN;
+            }
+        }
+    }
+}
+
+void process_snap_chunk(int i, char *output_dir, char *name, char *lvl, int *SnapChunk, int NSnapInChunk, int Nchunk_id,
+                           long long **DiskIDsChunkList, long long *DiskIDsChunkListNumPer,
+                           long long **BulgeIDsChunkList, long long *BulgeIDsChunkListNumPer){
+    // i is the snap chunk idx
+    // j loops through the disk id and bulge id chunks
+    // we output into data/phase_space_name/tmp"i"/tmp"j".hdf5
+    int j;
+    char prefix[1000], fname[1000];
+    uint NumPart_Total[NTYPES];
+    struct stat st = {0};
+    sprintf(prefix, "data/%s-%s/tmp%d", name, lvl, i);
+    if (stat(prefix, &st) == -1) {
+        mkdir(prefix, 0700);
+    }
+
+    double *Time;
+    Time = (double *)malloc(sizeof(double) * NSnapInChunk);
+
+    double **DiskPos, **DiskVel, **DiskAcc;
+    double **BulgePos, **BulgeVel, **BulgeAcc;
+    DiskPos = (double **)malloc(sizeof(double *) * Nchunk_id);
+    DiskVel = (double **)malloc(sizeof(double *) * Nchunk_id);
+    DiskAcc = (double **)malloc(sizeof(double *) * Nchunk_id);
+    BulgePos = (double **)malloc(sizeof(double *) * Nchunk_id);
+    BulgeVel = (double **)malloc(sizeof(double *) * Nchunk_id);
+    BulgeAcc = (double **)malloc(sizeof(double *) * Nchunk_id);
+    // for(int ii=0; ii<Nchunk_id; ii++){
+    //     DiskPos[ii] = (double **)malloc(sizeof(double *) * NSnapInChunk);
+    //     DiskVel[ii] = (double **)malloc(sizeof(double *) * NSnapInChunk);
+    //     DiskAcc[ii] = (double **)malloc(sizeof(double *) * NSnapInChunk);
+    //     BulgePos[ii] = (double **)malloc(sizeof(double *) * NSnapInChunk);
+    //     BulgeVel[ii] = (double **)malloc(sizeof(double *) * NSnapInChunk);
+    //     BulgeAcc[ii] = (double **)malloc(sizeof(double *) * NSnapInChunk);
+    //     for(int jj=0; jj<NSnapInChunk; jj++){
+    //         DiskPos[ii][jj] = (double *)malloc(sizeof(double) * 3 * DiskIDsChunkListNumPer[ii]);
+    //         DiskVel[ii][jj] = (double *)malloc(sizeof(double) * 3 * DiskIDsChunkListNumPer[ii]);
+    //         DiskAcc[ii][jj] = (double *)malloc(sizeof(double) * 3 * DiskIDsChunkListNumPer[ii]);
+    //         BulgePos[ii][jj] = (double *)malloc(sizeof(double) * 3 * BulgeIDsChunkListNumPer[ii]);
+    //         BulgeVel[ii][jj] = (double *)malloc(sizeof(double) * 3 * BulgeIDsChunkListNumPer[ii]);
+    //         BulgeAcc[ii][jj] = (double *)malloc(sizeof(double) * 3 * BulgeIDsChunkListNumPer[ii]);
+    //     }
+    // }
+    for(int ii=0; ii<Nchunk_id; ii++){
+        DiskPos[ii] = (double *)malloc(sizeof(double) * NSnapInChunk * 3 * DiskIDsChunkListNumPer[ii]);
+        DiskVel[ii] = (double *)malloc(sizeof(double) * NSnapInChunk * 3 * DiskIDsChunkListNumPer[ii]);
+        DiskAcc[ii] = (double *)malloc(sizeof(double) * NSnapInChunk * 3 * DiskIDsChunkListNumPer[ii]);
+        BulgePos[ii] = (double *)malloc(sizeof(double) * NSnapInChunk * 3 * BulgeIDsChunkListNumPer[ii]);
+        BulgeVel[ii] = (double *)malloc(sizeof(double) * NSnapInChunk * 3 * BulgeIDsChunkListNumPer[ii]);
+        BulgeAcc[ii] = (double *)malloc(sizeof(double) * NSnapInChunk * 3 * BulgeIDsChunkListNumPer[ii]);
+    }
+
+    // load in the snapshots
+    struct Part *DiskPart, *BulgePart;
+    double this_time;
+    hid_t file_id;
+    long long offset_disk[Nchunk_id], offset_bulge[Nchunk_id];
+    for(j=0; j<Nchunk_id; j++){
+        offset_disk[j] = 0;
+        offset_bulge[j] = 0;
+    }
+
+    double *dpos_off, *dvel_off, *dacc_off;
+    double *bpos_off, *bvel_off, *bacc_off;
+    for(j=0; j<NSnapInChunk; j++){
+        // read necessary header attributes
+        sprintf(fname, "%s/snapdir_%03d/snapshot_%03d.0.hdf5", output_dir, SnapChunk[j], SnapChunk[j]);
+        file_id = H5Fopen(fname, H5F_ACC_RDONLY, H5P_DEFAULT);
+        read_header_attribute(file_id, H5T_NATIVE_UINT, "NumPart_Total", NumPart_Total);
+        read_header_attribute(file_id, H5T_NATIVE_DOUBLE, "Time", &this_time);
+        H5Fclose(file_id);
+        Time[j] = this_time;
+        printf("processing snap %d of %d, time=%g\n", j, NSnapInChunk, Time[j]);
+
+        // load in the snapshots
+        get_part(output_dir, SnapChunk[j], 2, &DiskPart);
+        get_part(output_dir, SnapChunk[j], 3, &BulgePart);
+
+        // sort by ID
+        qsort(DiskPart, NumPart_Total[2], sizeof(struct Part), cmprID);
+        qsort(BulgePart, NumPart_Total[3], sizeof(struct Part), cmprID);
+
+        // now loop through chunks and sort pos and vel into output
+        for(int k=0; k<Nchunk_id; k++){
+            dpos_off = &(DiskPos[k][offset_disk[k]]);
+            dvel_off = &(DiskVel[k][offset_disk[k]]);
+            dacc_off = &(DiskAcc[k][offset_disk[k]]);
+            bpos_off = &(BulgePos[k][offset_bulge[k]]);
+            bvel_off = &(BulgeVel[k][offset_bulge[k]]);
+            bacc_off = &(BulgeAcc[k][offset_bulge[k]]);
+            sort_by_id(DiskIDsChunkList[k], DiskIDsChunkListNumPer[k], DiskPart, 
+                       &dpos_off, &dvel_off, &dacc_off);
+            sort_by_id(BulgeIDsChunkList[k], BulgeIDsChunkListNumPer[k], BulgePart, 
+                       &bpos_off, &bvel_off, &bacc_off);
+            offset_disk[k] += 3 * DiskIDsChunkListNumPer[k];
+            offset_bulge[k] += 3 * BulgeIDsChunkListNumPer[k];
+        }
+        
+        free(DiskPart);
+        free(BulgePart);
+    }
+
+    // now loop through each ID chunk and write to a temporary file
+    hsize_t disk_dims[3], bulge_dims[3], time_dims[2];
+    disk_dims[0] = bulge_dims[0] = time_dims[0] = NSnapInChunk;
+    disk_dims[2] = bulge_dims[2] = 3;
+    time_dims[1];
+    hid_t grp_disk, grp_bulge, disk_dspace_vec, bulge_dspace_vec, time_dspace, dset;
+    for(j=0; j<Nchunk_id; j++){
+        disk_dims[1] = DiskIDsChunkListNumPer[j];
+        bulge_dims[1] = BulgeIDsChunkListNumPer[j];
+
+        sprintf(fname, "%s/tmp%d.hdf5", prefix, j);
+        file_id = H5Fcreate(fname, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+        if(file_id < 0)
+            printf("UNABLE TO CREATE FILE %s", fname);
+
+        grp_disk = H5Gcreate1(file_id, "PartType2", 0);
+        grp_bulge = H5Gcreate1(file_id, "PartType3", 0);
+
+        disk_dspace_vec = H5Screate_simple(3, disk_dims, NULL);
+        bulge_dspace_vec = H5Screate_simple(3, bulge_dims, NULL);
+        time_dspace = H5Screate_simple(1, time_dims, NULL);
+
+        // Write time.
+        dset = H5Dcreate1(file_id, "Time", H5T_NATIVE_DOUBLE, time_dspace, H5P_DEFAULT);
+        H5Dwrite(dset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, Time);
+        H5Dclose(dset);
+
+        // Write disk coordinates.
+        dset = H5Dcreate1(grp_disk, "Coordinates", H5T_NATIVE_DOUBLE, disk_dspace_vec, H5P_DEFAULT);
+        H5Dwrite(dset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, DiskPos[j]);
+        H5Dclose(dset);
+
+        // Write disk velocities.
+        dset = H5Dcreate1(grp_disk, "Velocities", H5T_NATIVE_DOUBLE, disk_dspace_vec, H5P_DEFAULT);
+        H5Dwrite(dset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, DiskVel[j]);
+        H5Dclose(dset);
+
+        // Write disk accelerations.
+        dset = H5Dcreate1(grp_disk, "Acceleration", H5T_NATIVE_DOUBLE, disk_dspace_vec, H5P_DEFAULT);
+        H5Dwrite(dset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, DiskAcc[j]);
+        H5Dclose(dset);
+
+        // Write bulge coordinates.
+        dset = H5Dcreate1(grp_bulge, "Coordinates", H5T_NATIVE_DOUBLE, bulge_dspace_vec, H5P_DEFAULT);
+        H5Dwrite(dset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, BulgePos[j]);
+        H5Dclose(dset);
+
+        // Write bulge velocities.
+        dset = H5Dcreate1(grp_bulge, "Velocities", H5T_NATIVE_DOUBLE, bulge_dspace_vec, H5P_DEFAULT);
+        H5Dwrite(dset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, BulgeVel[j]);
+        H5Dclose(dset);
+
+        // Write bulge acclerations.
+        dset = H5Dcreate1(grp_bulge, "Acceleration", H5T_NATIVE_DOUBLE, bulge_dspace_vec, H5P_DEFAULT);
+        H5Dwrite(dset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, BulgeAcc[j]);
+        H5Dclose(dset);
+
+        H5Gclose(grp_disk);
+        H5Gclose(grp_bulge);
+        H5Fclose(file_id);
+    }
+    free(DiskPos);
+    free(DiskVel);
+    free(DiskAcc);
+    free(BulgePos);
+    free(BulgeVel);
+    free(BulgeAcc);
+
+    printf("finished with chunk %d\n", i);
 }
 
 int main(int argc, char* argv[]) {
@@ -337,20 +544,22 @@ int main(int argc, char* argv[]) {
     sprintf(fname, "%s/snapdir_%03d/snapshot_%03d.0.hdf5", output_dir, 0, 0);
     hid_t file_id = H5Fopen(fname, H5F_ACC_RDONLY, H5P_DEFAULT);
     read_header_attribute(file_id, H5T_NATIVE_UINT, "NumPart_Total", NumPart_Total);
+    read_header_attribute(file_id, H5T_NATIVE_UINT, "NumPart_Total", NumPart_Total_LastSnap);
     H5Fclose(file_id);
 
     // Pull out particles from the last snapshot
     struct Part *DiskPart, *BulgePart;
-    get_part(basepath, Nsnap-1, 2, &DiskPart);
-    get_part(basepath, Nsnap-1, 3, &BulgePart);
+    get_part(output_dir, Nsnap-1, 2, &DiskPart);
+    get_part(output_dir, Nsnap-1, 3, &BulgePart);
 
-    qsort(DiskPart, NumPart_Total[2], sizeof(struct Part), cmprID);
-    qsort(BulgePart, NumPart_Total[3], sizeof(struct Part), cmprID);
+    // sort particles
+    qsort(DiskPart, NumPart_Total_LastSnap[2], sizeof(struct Part), cmprID);
+    qsort(BulgePart, NumPart_Total_LastSnap[3], sizeof(struct Part), cmprID);
 
     // construct sorted ID list
     long long *DiskIDs, *BulgeIDs;
-    DiskIDs = get_IDs_from_part(DiskPart, NumPart_Total[2]);
-    BulgeIDs = get_IDs_from_part(BulgePart, NumPart_Total[3]);
+    DiskIDs = get_IDs_from_part(DiskPart, NumPart_Total_LastSnap[2]);
+    BulgeIDs = get_IDs_from_part(BulgePart, NumPart_Total_LastSnap[3]);
     
     int * SnapList;
     SnapList = (int *)malloc(sizeof(int) * Nsnap);
@@ -365,17 +574,35 @@ int main(int argc, char* argv[]) {
     // Create IDs chunked arrays
     long long **DiskIDsChunkList, **BulgeIDsChunkList;
     long long *DiskIDsChunkListNumPer, *BulgeIDsChunkListNumPer;
-    array_split_llong(Nchunk_id, DiskIDs, NumPart_Total[2], &DiskIDsChunkList, &DiskIDsChunkListNumPer);
-    array_split_llong(Nchunk_id, BulgeIDs, NumPart_Total[2], &BulgeIDsChunkList, &BulgeIDsChunkListNumPer);
+    array_split_llong(Nchunk_id, DiskIDs, NumPart_Total_LastSnap[2], &DiskIDsChunkList, &DiskIDsChunkListNumPer);
+    array_split_llong(Nchunk_id, BulgeIDs, NumPart_Total_LastSnap[3], &BulgeIDsChunkList, &BulgeIDsChunkListNumPer);
+
+    // Create output data directory if it doesn't exist
+    struct stat st = {0};
+    if (stat("./data", &st) == -1) {
+        mkdir("./data", 0700);
+    }
+    char data_dir[1000];
+    sprintf(data_dir, "./data/%s-%s", name, lvl);
+    if (stat(data_dir, &st) == -1) {
+        mkdir(data_dir, 0700);
+    }
+
+    for(int i=0; i<Nchunk_snap; i++){
+        process_snap_chunk(i, output_dir, name, lvl, SnapChunkList[i], SnapChunkListNumPer[i], Nchunk_id,
+                           DiskIDsChunkList, DiskIDsChunkListNumPer,
+                           BulgeIDsChunkList, BulgeIDsChunkListNumPer);
+    }
 
     int i;
     i = 0;
 
-    printf("DiskIDs[0]=%lld, DiskIDs[100]=%lld, DiskIDs[last]=%lld\n", DiskPart[0].ID, DiskPart[100].ID, DiskPart[NumPart_Total[2]-1].ID);
-    printf("BulgeIDs[0]=%lld, BulgeIDs[100]=%lld, BulgeIDs[last]=%lld\n", BulgePart[0].ID, BulgePart[100].ID, BulgePart[NumPart_Total[3]-1].ID);
 
-    printf("DiskIDs[0]=%lld, DiskIDs[100]=%lld, DiskIDs[last]=%lld\n", DiskIDs[0], DiskIDs[100], DiskIDs[NumPart_Total[2]-1]);
-    printf("BulgeIDs[0]=%lld, BulgeIDs[100]=%lld, BulgeIDs[last]=%lld\n", BulgeIDs[0], BulgeIDs[100], BulgeIDs[NumPart_Total[3]-1]);
+    printf("DiskIDs[0]=%lld, DiskIDs[100]=%lld, DiskIDs[last]=%lld\n", DiskPart[0].ID, DiskPart[100].ID, DiskPart[NumPart_Total_LastSnap[2]-1].ID);
+    printf("BulgeIDs[0]=%lld, BulgeIDs[100]=%lld, BulgeIDs[last]=%lld\n", BulgePart[0].ID, BulgePart[100].ID, BulgePart[NumPart_Total_LastSnap[3]-1].ID);
+
+    printf("DiskIDs[0]=%lld, DiskIDs[100]=%lld, DiskIDs[last]=%lld\n", DiskIDs[0], DiskIDs[100], DiskIDs[NumPart_Total_LastSnap[2]-1]);
+    printf("BulgeIDs[0]=%lld, BulgeIDs[100]=%lld, BulgeIDs[last]=%lld\n", BulgeIDs[0], BulgeIDs[100], BulgeIDs[NumPart_Total_LastSnap[3]-1]);
 
     printf("part[0] ID=%lld, Pos=%g|%g|%g, Vel=%g|%g|%g, Acc=%g|%g|%g\n", BulgePart[i].ID, BulgePart[i].Pos[0], BulgePart[i].Pos[1], BulgePart[i].Pos[2],
                                                                           BulgePart[i].Vel[0], BulgePart[i].Vel[1], BulgePart[i].Vel[2], 
