8c691aeed5266422fe2a39cc25a8efc9f4ece29c
Gus Beane <angus.beane@cfa.harvard.edu>
2022-02-16 14:43:27 -0500

    Create load-modules.sh

--- plots/load-modules.sh
+++ plots/load-modules.sh
@@ -1,6 +1,8 @@
 module purge
-module load gcc/8.2.0-fasrc01 
-module load openmpi/4.0.1-fasrc01
+#module load gcc/8.2.0-fasrc01 
+#module load openmpi/4.0.1-fasrc01
+module load gcc/7.1.0-fasrc01
+module load openmpi/3.1.3-fasrc01
 module load gsl
 module load hdf5
 module load python/3.6.3-fasrc02
