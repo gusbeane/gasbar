0fd620dbe9403b270850d09e1731af6c553832b6
paper-maker <paper-maker>
2022-04-28 15:39:13 +0000

    update pdf

---
+++ plots/radial_gasflow_rate/job_gasflow.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#SBATCH -p hernquist,conroy
+##SBATCH --constraint=intel
+#SBATCH -J gasflow
+#SBATCH -n 64
+#SBATCH -N 1
+#SBATCH -o OUTPUT_frames.%j.out
+#SBATCH -e ERROR_frames.%j.err
+##SBATCH --exclusive
+#SBATCH --mail-user=angus.beane@cfa.harvard.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mem-per-cpu=3900
+##SBATCH -t 4-00:00           # Runtime in D-HH:MM
+#SBATCH -t 7-00:00           # Runtime in D-HH:MM
+
+source ../load-modules.sh
+
+ulimit -c unlimited
+
+python3 compute_radial_gasflow.py ${SLURM_NTASKS} $1
+
