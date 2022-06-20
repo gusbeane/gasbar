b8589a02eeea6cdc5e41ef667ce99a00f520ac8c
paper-maker <paper-maker>
2022-06-20 23:42:57 +0000

    update pdf

---
+++ movies/movie_corot/job_movie.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#SBATCH -p hernquist,conroy
+#SBATCH -J movie 
+#SBATCH -n 64 
+#SBATCH -N 1
+#SBATCH -o OUTPUT_frames.%j.out
+#SBATCH -e ERROR_frames.%j.err
+##SBATCH --exclusive
+#SBATCH --mail-user=angus.beane@cfa.harvard.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mem-per-cpu=3900
+##SBATCH -t 4-00:00           # Runtime in D-HH:MM
+#SBATCH -t 0-12:00           # Runtime in D-HH:MM
+
+source ../load-modules.sh
+
+ulimit -c unlimited
+
+python3 run_projections.py ${SLURM_NTASKS}
+
