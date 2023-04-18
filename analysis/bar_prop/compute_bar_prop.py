f4b1358fbe5f17ad3c00e7353fe51e8012d744ce
Gus Beane <angus.beane@cfa.harvard.edu>
2023-04-18 10:53:33 -0400

    Fix in_bar reading and remove old fn

--- analysis/bar_prop/compute_bar_prop.py
+++ analysis/bar_prop/compute_bar_prop.py
@@ -18,73 +18,6 @@ def read_snap(path, idx, parttype=[0], fields=['Coordinates', 'Masses', 'Velocit
     
     return arepo.Snapshot(fname, idx, parttype=parttype, fields=fields, combineFiles=True)
 
-def compute_bar_properties(name, mass, in_bar_out, center):
-    nsnap = np.shape(in_bar_out[0])[0]
-    idx_list = np.arange(0, nsnap, 50)
-
-    phase_space_path='/n/home01/abeane/starbar/plots/phase_space/data/'
-    nchunk = len(glob.glob(phase_space_path+name+'/phase_space_'+name+'.*.hdf5'))
-
-    bar_prop_list = np.zeros((len(idx_list), 5))
-
-    ctr = 0
-    for i in tqdm(range(nchunk)):
-        in_bar = in_bar_out[i]
-        Mbar = 0.0
-        Lzbar = 0.0
-    
-        fin = phase_space_path + name + '/phase_space_' + name + '.' + str(i) + '.hdf5'
-        h5in = h5.File(fin, mode='r')
-    
-        tlist = np.array(h5in['Time'])
-        pos_tot = np.array(h5in['Coordinates']) - center
-        vel_tot = np.array(h5in['Velocities'])
-        ids = np.array(h5in['ParticleIDs'])
-    
-        if i==0:
-            Rlist = {}
-            for idx in idx_list:
-                Rlist[idx] = np.array([])
-    
-        for j,idx in enumerate(idx_list):
-            pos = pos_tot[:,idx,:]
-            vel = vel_tot[:,idx,:]
-        
-            key = in_bar[idx][ctr:ctr+len(ids)]
-        
-        
-            pos_inbar = pos[key]
-            vel_inbar = vel[key]
-
-            N_inbar = len(np.where(key)[0])
-    
-            Mbar = mass * len(pos_inbar)
-            Lzbar = mass * np.sum(np.cross(pos_inbar, vel_inbar)[:,2])
-        
-            bar_prop_list[j,0] = tlist[idx]
-            bar_prop_list[j,3] += Mbar
-            bar_prop_list[j,4] += Lzbar
-        
-            if N_inbar == 0:
-                R = np.array([])
-            elif N_inbar == 1:
-                R = np.array([np.sqrt(pos_inbar[0][0]**2 + pos_inbar[0][1]**2)]).reshape((1,))
-            else:
-                R = np.linalg.norm(pos_inbar[:,:2], axis=1)
-            Rlist[idx] = np.concatenate((Rlist[idx], R))
-    
-        ctr += len(ids)
-    
-    
-    for j,idx in enumerate(idx_list):
-        if len(Rlist[idx]>100):
-            Rbar = np.percentile(Rlist[idx], 99)
-        else:
-            Rbar = 0.0
-        bar_prop_list[j,2] = Rbar
-    
-    return bar_prop_list
-
 def get_mass_and_center_from_name(name):
     if 'lvl5' in name:
         mass = 6E-6 * 8.0
@@ -111,31 +44,33 @@ def _bar_prop_one_chunk(prefix_in_bar, prefix_phase_space, name, chunk_idx):
     h5_phase_space = h5.File(fname_phase_space, mode='r')
 
     # pull out in_bar from file
-    in_bar = np.array(h5_in_bar['in_bar'])
-    idx_list = np.array(h5_in_bar['idx_list'])
-    
+    in_bar = np.array(h5_in_bar['PartType2/in_bar'])
+    idx_list = np.array(h5_in_bar['PartType2/idx_list'])
+
+    in_bar = np.concatenate((in_bar, h5_in_bar['PartType3/in_bar']), axis=1)
+    if 'PartType4' in h5_in_bar.keys():
+        in_bar = np.concatenate((in_bar, h5_in_bar['PartType4/in_bar']), axis=1)
+
     bar_prop_list = {}
     for key in ['tlist', 'bar_frac', 'Rbar', 'Mbar', 'Lzbar', 'Izbar']:
         bar_prop_list[key] = np.zeros(len(idx_list))
     
-    print(len(idx_list))
-
     # get mass and center from name
     mass, center = get_mass_and_center_from_name(name)
 
     # load phase space properties
     # load disk particles
-    pos_tot = np.array(h5_phase_space['PartType2/Coordinates']).swapaxes(0, 1)
-    vel_tot = np.array(h5_phase_space['PartType2/Velocities']).swapaxes(0, 1)
+    pos_tot = np.array(h5_phase_space['PartType2/Coordinates'])
+    vel_tot = np.array(h5_phase_space['PartType2/Velocities'])
 
     # load bulge particles
-    pos_tot = np.concatenate((pos_tot, np.array(h5_phase_space['PartType3/Coordinates']).swapaxes(0, 1) ))
-    vel_tot = np.concatenate((vel_tot, np.array(h5_phase_space['PartType3/Velocities']).swapaxes(0, 1) ))
+    pos_tot = np.concatenate((pos_tot, np.array(h5_phase_space['PartType3/Coordinates']) ))
+    vel_tot = np.concatenate((vel_tot, np.array(h5_phase_space['PartType3/Velocities']) ))
 
     # load star particles (if they exist)
     if 'PartType4' in h5_phase_space.keys():
-        pos_tot = np.concatenate((pos_tot, np.array(h5_phase_space['PartType4/Coordinates']).swapaxes(0, 1) ))
-        vel_tot = np.concatenate((vel_tot, np.array(h5_phase_space['PartType4/Velocities']).swapaxes(0, 1) ))
+        pos_tot = np.concatenate((pos_tot, np.array(h5_phase_space['PartType4/Coordinates']) ))
+        vel_tot = np.concatenate((vel_tot, np.array(h5_phase_space['PartType4/Velocities']) ))
 
     pos_tot = pos_tot - center
     tlist = np.array(h5_phase_space['Time'])
@@ -236,15 +171,15 @@ def get_other_output(prefix_in_bar, name):
     fin = prefix_in_bar + 'in_bar_' + name + '.0.hdf5'
     h5in = h5.File(fin, mode='r')
 
-    tlist = np.array(h5in['tlist'])
-    idx_list = np.array(h5in['idx_list'])
+    tlist = np.array(h5in['PartType2/tlist'])
+    idx_list = np.array(h5in['PartType2/idx_list'])
     bar_angle = np.array(h5in['bar_angle'])
 
     return tlist, idx_list, bar_angle
 
-def run(name, nproc, basepath = '/n/home01/abeane/starbar/plots/bar_orbits/data/'):
+def run(name, nproc, basepath = '/n/holylfs05/abeane/gasbar/analysis/bar_orbits/data/'):
     prefix_in_bar = '../in_bar/data/in_bar_' + name + '/'
-    prefix_phase_space = '/n/home01/abeane/starbar/plots/phase_space/data/' + name + '/'
+    prefix_phase_space = '/n/holylfs05/LABS/hernquist_lab/Users/abeane/gasbar/analysis/phase_space/data/' + name + '/'
 
     nchunk = len(glob.glob(prefix_in_bar+'/in_bar_'+name+'.*.hdf5'))
 
